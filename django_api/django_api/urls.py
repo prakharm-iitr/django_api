2df590ca958697c029076467587717206d0d3f4a
prakharm-iitr <prakharmishra.iitr@gmail.com>
2020-09-28 21:32:28 +0530

    Add REST JWT

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("accounts.urls")),
-    path('accounts/', include('django.contrib.auth.urls'))
+    # path('', include("accounts.urls")),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('api-token-auth/', obtain_jwt_token),
 ]
