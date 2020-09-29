1e00ddb7a4a131e38abaf3806577afcce56c12e2
prakharm-iitr <prakharmishra.iitr@gmail.com>
2020-09-29 07:13:22 +0530

    Procfile

--- django_api/accounts/urls.py
+++ django_api/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.create_user, name='register'),
+    path('', views.create_user, name='register'),
     path('api-token-auth/', views.login, name='login'),
 ]
